773dbdaea3aea82d43046d2debc066d4fd5fd53c
PatelPratik39 <ptl.pratik717@gmail.com>
2024-06-21 11:40:22 -0500

    New Transaction demo project

---
+++ springboot-transaction-demo/src/main/java/com/ecommerce/springboot_transaction_demo/SpringbootTransactionDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.springboot_transaction_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTransactionDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTransactionDemoApplication.class, args);
+	}
+
+}
---
+++ springboot-transaction-demo/src/test/java/com/ecommerce/springboot_transaction_demo/SpringbootTransactionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.springboot_transaction_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTransactionDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
