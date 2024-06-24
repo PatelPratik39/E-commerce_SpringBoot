23570831897b340a69cd993246c6be295b1fb78b
PatelPratik39 <ptl.pratik717@gmail.com>
2024-06-24 01:14:50 -0500

    Junit test cases passed for Update Employee, find By Id and findByEmail

--- spring-boot-testing/src/main/java/com/ecommerce/spring_boot_testing/repository/EmployeeRepository.java
+++ spring-boot-testing/src/main/java/com/ecommerce/spring_boot_testing/repository/EmployeeRepository.java
@@ -3,7 +3,10 @@ package com.ecommerce.spring_boot_testing.repository;
 import com.ecommerce.spring_boot_testing.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EmployeeRepository extends JpaRepository< Employee, Long > {
 
+    Optional<Employee> findByEmail(String email);
 
 }
--- spring-boot-testing/src/test/java/com/ecommerce/spring_boot_testing/repository/EmployeeRepositoryTests.java
+++ spring-boot-testing/src/test/java/com/ecommerce/spring_boot_testing/repository/EmployeeRepositoryTests.java
@@ -76,8 +76,45 @@ public class EmployeeRepositoryTests {
 
     //        then - verify the output
             assertThat(employeeDB).isNotNull();
-//            assertThat()
+
         }
 
+        //    JUnit test for
+            @DisplayName("JUnite test for get employee by Email operation")
+            @Test
+            public void givenEmployeeEmail_whenFindByEMail_thenEmployeeObject(){
+
+        //        Given - Pre-condition or setup test
+                Employee employee = Employee.builder().firstName("Mangoo").lastName("Moti").email("mangoo@email.com").build();
+                employeeRepository.save(employee);
+
+        //        When - action or the behavior that we are going to test
+                Employee employeeDB = employeeRepository.findByEmail(employee.getEmail()).get();
+
+        //        then - verify the output
+                assertThat(employeeDB).isNotNull();
+
+            }
+//    Update Employee Junit test case
+    //    JUnit test for
+        @DisplayName("Update Employee Junit test case")
+        @Test
+        public void givenemployeeObject_whenUpdatedEmployee_thenReturnUpdatedEmployee(){
+
+    //        Given - Pre-condition or setup test
+            Employee employee = Employee.builder().firstName("Mangoo").lastName("Moti").email("mangoo@email.com").build();
+            employeeRepository.save(employee);
+
+    //        When - action or the behavior that we are going to test
+            Employee savedEmployee = employeeRepository.findById(employee.getId()).get();
+            savedEmployee.setEmail("mangooMoti@email.com");
+            savedEmployee.setFirstName("Mangoo Gondi");
+            Employee updatedEmployee =  employeeRepository.save(savedEmployee);
+    //        then - verify the output
+            assertThat(updatedEmployee).isNotNull();
+            assertThat(updatedEmployee.getEmail()).isEqualTo("mangooMoti@email.com");
+            assertThat(updatedEmployee.getFirstName()).isEqualTo("Mangoo Gondi");
+
+        }
 
 }
