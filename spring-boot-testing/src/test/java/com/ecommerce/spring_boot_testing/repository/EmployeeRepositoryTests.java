9eb50b1edf8fe41c98c6b34377ed6b18ed90e878
PatelPratik39 <ptl.pratik717@gmail.com>
2024-06-24 00:49:26 -0500

    Junit test cases passed

--- spring-boot-testing/src/test/java/com/ecommerce/spring_boot_testing/repository/EmployeeRepositoryTests.java
+++ spring-boot-testing/src/test/java/com/ecommerce/spring_boot_testing/repository/EmployeeRepositoryTests.java
@@ -1,13 +1,17 @@
 package com.ecommerce.spring_boot_testing.repository;
 
 import com.ecommerce.spring_boot_testing.model.Employee;
-import lombok.AllArgsConstructor;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import com.ecommerce.spring_boot_testing.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @DataJpaTest
 public class EmployeeRepositoryTests {
@@ -20,7 +24,7 @@ public class EmployeeRepositoryTests {
     Junit test for save Employee operation
      */
 
-    @DisplayName("Junit test for save Employee Operation")
+//    @DisplayName("Junit test for save Employee Operation")
     @Test
     public void givenEmployeeObject_whenSave_thenReturnSavedEmployee(){
 
@@ -31,12 +35,49 @@ public class EmployeeRepositoryTests {
         Employee savedEmployee = employeeRepository.save(employee);
 
 //        then - verify the output
-        Assertions.assertThat(savedEmployee).isNotNull();
-        Assertions.assertThat(savedEmployee.getId()).isGreaterThan(0);
+        assertThat(savedEmployee).isNotNull();
+        assertThat(savedEmployee.getId()).isGreaterThan(0);
+    }
+//    JUnit test for
+    @DisplayName("Create Employee and getAll from repository")
+    @Test
+    public void givenEmployeeList_whenFindAll_thenEmployeeList(){
 
+//        Given - Pre-condition or setup test
+        Employee employee = Employee.builder().firstName("Mangoo").lastName("Moti").email("mangoo@email.com").build();
+        Employee employee1 = Employee.builder().firstName("Chaman").lastName("zingoor").email("chaman@email.com").build();
+        Employee employee2 = Employee.builder().firstName("Lallu Lal").lastName("Singh").email("lallu@email.com").build();
+        Employee employee3 = Employee.builder().firstName("Chucha Singh").lastName("Bhatto").email("chucha@email.com").build();
+        employeeRepository.save(employee1);
+        employeeRepository.save(employee);
+        employeeRepository.save(employee2);
+        employeeRepository.save(employee3);
+
+//        When - action or the behavior that we are going to test
+        List <Employee> employeeList = employeeRepository.findAll();
+
+//        then - verify the output
+        assertThat(employeeList).isNotNull();
+        assertThat(employeeList.size()).isEqualTo(3);
 
     }
 
+    //    JUnit test for
+        @DisplayName(" Junit test for get employee id operation")
+        @Test
+        public void givenEmployeeObject_whenFindById_thenReturnEmployeeObject(){
+
+    //        Given - Pre-condition or setup test
+            Employee employee = Employee.builder().firstName("Mangoo").lastName("Moti").email("mangoo@email.com").build();
+            employeeRepository.save(employee);
+
+    //        When - action or the behavior that we are going to test
+            Employee employeeDB = employeeRepository.findById(employee.getId()).get();
+
+    //        then - verify the output
+            assertThat(employeeDB).isNotNull();
+//            assertThat()
+        }
 
 
 }
