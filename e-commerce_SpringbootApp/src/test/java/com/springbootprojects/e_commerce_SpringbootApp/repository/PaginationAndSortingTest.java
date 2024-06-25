d1f9413601cdc6cd8a91009274eb7694ea50f5a0
PatelPratik39 <ptl.pratik717@gmail.com>
2024-06-24 21:25:10 -0500

    JUnit test case for Paging

---
+++ e-commerce_SpringbootApp/src/test/java/com/springbootprojects/e_commerce_SpringbootApp/repository/PaginationAndSortingTest.java
@@ -0,0 +1,62 @@
+package com.springbootprojects.e_commerce_SpringbootApp.repository;
+
+import com.springbootprojects.e_commerce_SpringbootApp.entity.Product;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+@SpringBootTest
+public class PaginationAndSortingTest {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Test
+    void pagination(){
+       int pageNo = 0;
+       int pageSize = 5;
+
+//       Create Pageable Object
+        Pageable pageable = PageRequest.of(pageNo,pageSize);
+
+//        findAll method and pass pageable instance
+        Page<Product> page = productRepository.findAll(pageable);
+
+        List<Product> products = page.getContent();
+
+//        Iterate of over Product
+        products.forEach((p) -> {
+            System.out.println(p);
+        });
+
+//        total pages
+        int totalPage = page.getTotalPages();
+
+//        total elements
+        long totalElements = page.getTotalElements();
+
+//        total of elements
+        int numberOfElements = page.getNumberOfElements();
+
+//        size
+        int size = page.getSize();
+
+//        last
+        boolean isLast = page.isLast();
+//        first
+        boolean isFirst = page.isFirst();
+        System.out.println("total Page -> " + totalPage);
+        System.out.println("totalElements -> " + totalElements);
+        System.out.println(" Number Of Elements -> " + numberOfElements);
+        System.out.println(" Size -> " + size);
+        System.out.println(" isLast -> " + isLast);
+        System.out.println(" isFirst -> "+ isFirst);
+
+    }
+
+}
